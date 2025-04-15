ad7a50b6a77e432f969f3f0a98cf2f86dcb45a48
Louise <louiseneves87@gmail.com>
2025-04-14 22:21:34 -0300

    Melhorias feitas no projeto

---
+++ script.js
@@ -0,0 +1,48 @@
+const botaoCalcular = document.getElementById('botaoCalcular');
+const resultadoDiv = document.getElementById('resultado');
+
+function calcularIMC() {
+    const peso = parseFloat(document.getElementById('peso').value);
+    const altura = parseFloat(document.getElementById('altura').value);
+
+    if (isNaN(peso) || isNaN(altura) || peso <= 0 || altura <= 0) {
+        resultadoDiv.textContent = 'Preencha os campos corretamente!';
+        resultadoDiv.className = '';
+        return;
+    }
+
+    const resultadoIMC = (peso / (altura * altura)).toFixed(2);
+    let mensagem = '';
+    let classeResultado = '';
+
+    if (resultadoIMC < 16) {
+        mensagem = 'Baixo peso muito grave';
+        classeResultado = 'baixo-peso-grave';
+    } else if (resultadoIMC < 17) {
+        mensagem = 'Baixo peso grave';
+        classeResultado = 'baixo-peso-grave';
+    } else if (resultadoIMC < 18.5) {
+        mensagem = 'Baixo peso';
+        classeResultado = 'baixo-peso';
+    } else if (resultadoIMC < 25) {
+        mensagem = 'Peso normal';
+        classeResultado = 'peso-normal';
+    } else if (resultadoIMC < 30) {
+        mensagem = 'Sobrepeso';
+        classeResultado = 'sobrepeso';
+    } else if (resultadoIMC < 35) {
+        mensagem = 'Obesidade grau 1';
+        classeResultado = 'obesidade-grau-1';
+    } else if (resultadoIMC < 40) {
+        mensagem = 'Obesidade grau 2';
+        classeResultado = 'obesidade-grau-2';
+    } else {
+        mensagem = 'Obesidade grau 3';
+        classeResultado = 'obesidade-grau-3';
+    }
+
+    resultadoDiv.textContent = `Seu IMC é ${resultadoIMC}! ${mensagem}`;
+    resultadoDiv.className = classeResultado;
+}
+
+botaoCalcular.addEventListener('click', calcularIMC);
